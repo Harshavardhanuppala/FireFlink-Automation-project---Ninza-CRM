c021d7cb4b0388fb89035021108e848985ae5ec7
Uppala Harshavardhan <Uppala Harshavardhan@FFE-Uppala-Harshavardhan>
2025-08-22 15:10:15 +0530

    Added a Get Details method in Database

--- src/main/java/ninzaCRM/genericUtility/DatabaseUtility.java
+++ src/main/java/ninzaCRM/genericUtility/DatabaseUtility.java
@@ -1,10 +1,12 @@
 package ninzaCRM.genericUtility;
 
+import java.awt.List;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.mysql.jdbc.Driver;
 
 public class DatabaseUtility 
 {
@@ -18,5 +20,11 @@ public class DatabaseUtility
 	public void closeConnection() throws SQLException
 	{
 		connection.close();
+	}
+	public ResultSet getUserDetails(String user) throws SQLException
+	{
+		PreparedStatement statement=connection.prepareStatement("select * from user_details where username='"+user+"'");
+		ResultSet set=statement.executeQuery();
+		return set;
 	}
 }
