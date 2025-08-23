98d5902e38bcf14a82939459834dd23350d402dc
Uppala Harshavardhan <Uppala Harshavardhan@FFE-Uppala-Harshavardhan>
2025-08-23 11:10:41 +0530

    cahnging browser

--- src/main/java/ninzaCRM/genericUtility/DatabaseUtility.java
+++ src/main/java/ninzaCRM/genericUtility/DatabaseUtility.java
@@ -1,6 +1,5 @@
 package ninzaCRM.genericUtility;
 
-import java.awt.List;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -8,23 +7,60 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
+
 public class DatabaseUtility 
 {
-	private Connection connection;
-	public Connection getDBConnection() throws SQLException
-	{
-		connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/work", "root", "root");
-		return connection;
-	}
 	
-	public void closeConnection() throws SQLException
+	private static Connection connection;
+	/**
+	 * making the connection between java application and DB
+	 * @throws SQLException
+	 * @throws ClassNotFoundException 
+	 */
+	public static void getDBConnection() throws SQLException, ClassNotFoundException
+	{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/work", "root", "root");
+	}
+	/**
+	 * closing the connection betwen the java application and DB
+	 * @throws SQLException
+	 */
+	public static void closeConnection() throws SQLException
 	{
 		connection.close();
 	}
-	public ResultSet getUserDetails(String user) throws SQLException
+	/**
+	 * check the username is present in the DB or not
+	 * @param user
+	 * @throws SQLException
+	 */
+	public void checkUserDetails(String user) throws SQLException
 	{
+		
 		PreparedStatement statement=connection.prepareStatement("select * from user_details where username='"+user+"'");
 		ResultSet set=statement.executeQuery();
-		return set;
+		if(set.next())
+		{
+			System.out.println("Data if found");
+		}
+		else
+		{
+			System.out.println("Data is not found");
+			org.testng.Assert.fail();
+		}
+	}
+	/**
+	 * inserting the default details from the user.
+	 * @throws SQLException
+	 */
+	public void insertUserDetails() throws SQLException
+	{
+		PreparedStatement statement=connection.prepareStatement("insert into user_details values('harsha','harsha','uppala','Male','123456',1");
+		int set=statement.executeUpdate();
+		if(set!=0)
+			System.out.println("details added");
+		else
+			System.out.println("Details not added");
 	}
 }
